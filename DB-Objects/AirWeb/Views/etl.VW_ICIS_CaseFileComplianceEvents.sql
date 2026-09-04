309591fd8d6c42427186d28eaa88da67cd8a6e84
Doug Waldron <douglas.waldron@dnr.ga.gov>
2026-09-04 10:24:24 -0400

    Fix bug in case file/compliance event linkage

--- DB-Objects/AirWeb/Views/etl.VW_ICIS_CaseFileComplianceEvents.sql
+++ DB-Objects/AirWeb/Views/etl.VW_ICIS_CaseFileComplianceEvents.sql
@@ -16,6 +16,7 @@ When        Who                 What
 2026-01-29  DWaldron            Initial version (epa-dx#2)
 2026-02-27  DWaldron            Only submit "reportable" Compliance Events (air-web#502)
 2026-03-16  DWaldron            Rename the Case Files table (epa-dx#95)
+2026-09-04  DWaldron            Fix bug in compliance work join (epa-dx#108)
 
 ***************************************************************************************************/
 
@@ -27,13 +28,12 @@ from dbo.CaseFileComplianceEvents v
     inner join dbo.EnforcementCaseFiles f
         on f.Id = v.CaseFileId
     inner join dbo.ComplianceWork c
-        on c.Id = v.CaseFileId
+        on c.Id = v.ComplianceEventId
 where f.IsDeleted = 0
   and f.ActionNumber is not null
   and c.IsDeleted = 0
   and c.ActionNumber is not null
   and c.IsClosed = 1
-  --// Could probably be simplified as `<> N'Inspection'`
   and c.ComplianceWorkType in
       (N'AnnualComplianceCertification', N'Inspection', N'Report', N'SourceTestReview');
 
